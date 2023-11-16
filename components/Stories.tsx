568c9e2bbd64b30e74eafb1c72e0b4a479f48090
ishitaraina1807 <ishitaraina18@gmail.com>
2023-11-16 19:34:00 +0530

    components

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -7,22 +7,37 @@ export default function Stories() {
     return (
       <div className="mx-52 mt-40">
         <h1 className='text-[#5252D4] text-4xl font-extrabold '>stories</h1>
-        <div className="flex flex-col items-center">
+        <div className="flex justify-around">
+{/* story 1 */}
+<div className="flex flex-col items-center">
           <div className="bg-white rounded-full relative -mb-28 z-50">
             <Image src={stories1} alt="img" />
           </div>
-  
-          <div className="bg-gray-200 shadow-2xl rounded-xl w-96 h-96 pb-8 px-8 relative pt-24">
+          <div className="bg-gray-300 w-[384px] h-[362px] pb-8 px-8 relative pt-32">
             <div>
-              <p className="text-center bt-smalltitle font-bold pb-4">Name Surname</p>
-  
-              <p className="text-center bt-smalltext">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum placeat
-                aperiam tempora.
+              <p className="text-center bt-smalltitle font-bold pb-6">Prof Mac Sandman</p>
+              <p className="text-left text-lg font-normal bt-smalltext">
+              Lorem ipsum dolor sit amet consectetur. Ac lacinia sed orci mollis quisque tristique. Fermentum egestas ipsum ut sit.
               </p>
             </div>
           </div>
         </div>
+{/* story 2 */}
+      <div className="flex flex-col items-center">
+          <div className="bg-white rounded-full relative -mb-28 z-50">
+            <Image src={stories2} alt="img" />
+          </div>
+          <div className="bg-gray-300 w-[384px] h-[362px] pb-8 px-8 relative pt-32">
+            <div>
+              <p className="text-center bt-smalltitle font-bold pb-6">Prof Mac Sandman</p>
+              <p className="text-left text-lg font-normal bt-smalltext">
+              Lorem ipsum dolor sit amet consectetur. Ac lacinia sed orci mollis quisque tristique. Fermentum egestas ipsum ut sit.
+              </p>
+            </div>
+          </div>
+        </div>
+        </div>
+        
       </div>
     );
   }
